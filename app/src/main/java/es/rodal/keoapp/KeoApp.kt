e0388cacb701a38bea5b277d4ea97ac9f75b25bb
rodal <reulrodalsantome@gmail.com>
2024-07-09 23:48:00 +0200

    trying to request permission

--- app/src/main/java/es/rodal/keoapp/KeoApp.kt
+++ app/src/main/java/es/rodal/keoapp/KeoApp.kt
@@ -20,6 +20,7 @@ import android.app.Application
 import android.app.NotificationChannel
 import android.app.NotificationManager
 import android.content.Context
+import android.content.Intent
 import android.os.Build
 import dagger.hilt.android.HiltAndroidApp
 
--- app/src/main/java/es/rodal/keoapp/MainActivity.kt
+++ app/src/main/java/es/rodal/keoapp/MainActivity.kt
@@ -22,15 +22,20 @@ import android.app.NotificationManager
 import android.app.PendingIntent
 import android.content.Context
 import android.content.Intent
+import android.content.pm.PackageManager
 import android.os.Build
 import android.os.Bundle
+import android.provider.Settings
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.activity.result.ActivityResultLauncher
+import androidx.activity.result.contract.ActivityResultContracts
 import androidx.annotation.RequiresApi
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.ui.Modifier
+import androidx.core.content.ContextCompat
 import dagger.hilt.android.AndroidEntryPoint
 import es.rodal.keoapp.RecordatorioNotificationReceiver.Companion.NOTIFICATION_ID
 import es.rodal.keoapp.ui.MainNavigation
@@ -40,9 +45,22 @@ import java.util.Calendar
 @AndroidEntryPoint
 class MainActivity : ComponentActivity() {
 
-    @RequiresApi(Build.VERSION_CODES.M)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+        // Verificar y solicitar permiso al abrir la aplicación
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.S) {
+            val alarmManager = getSystemService(Context.ALARM_SERVICE) as AlarmManager
+            if (!alarmManager.canScheduleExactAlarms()) {
+                // Redirigir a la configuración para habilitar el permiso
+                val intent = Intent().apply {
+                    action = Settings.ACTION_REQUEST_SCHEDULE_EXACT_ALARM
+                }
+                startActivity(intent)
+            }
+        }
+
+        //https://developer.android.com/about/versions/14/changes/schedule-exact-alarms?hl=es-419
 
         setContent {
             MyApplicationTheme {
