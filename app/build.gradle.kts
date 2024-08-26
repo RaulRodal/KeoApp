df6a6e6f7eea2b31e217bca2173328582d368a7f
rodal <reulrodalsantome@gmail.com>
2024-08-26 23:30:58 +0200

    release 6

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -33,8 +33,8 @@ android {
         applicationId = "es.rodal.keoapp"
         minSdk = 23
         targetSdk = 34
-        versionCode = 4
-        versionName = "1.0.1"
+        versionCode = 6
+        versionName = "1.0.2"
 
         testInstrumentationRunner = "es.rodal.keoapp.HiltTestRunner"
         vectorDrawables {
